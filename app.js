73b400fd5bc96d9b77ca7f0ffcab3648b51839ef
unknown <fajarazkafadillah@yahoo.com>
2022-12-03 09:51:09 +0700

    add naskah proklamasi

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3009
 
 app.get('/', (req, res) => {
-  res.send('Hello Never!')
+  res.send('Proklamasi. Kami, Bangsa Indonesia dengan ini menjatakan kemerdekaan Indonesia. Hal2 yang mengenai pemindahan kekoeasaan d.l.l., diselenggarakan dengan tjara saksama dan dalam tempoh jang sesingkat-singkatnja. Djakarta, 17-08-05 wakil2 bangsa Indonesia,')
 })
 
 app.listen(port, () => {
